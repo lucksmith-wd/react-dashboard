ffa516a25f5345eb69029db8fdbf5308530b22b5
Balazs Braun <lucksmith.wd@gmail.com>
2024-03-23 11:07:59 +0800

    Finish Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
-import { Sidebar } from "./scenes/global/Sidebar";
-import { Dashboard } from "./scenes/dashboard";
-import { Team } from "./scenes/team";
-import { Invoices } from "./scenes/invoices";
-import { Contacts } from "./scenes/contacts";
-import { Bar } from "./scenes/bar";
-import { Form } from "./scenes/form";
-import { Line } from "./scenes/line";
-import { Pie } from "./scenes/pie";
-import { FAQ } from "./scenes/faq";
-import { Geography } from "./scenes/geography";
-import { Calendar } from "./scenes/calendar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import { Team } from "./scenes/team";
+// import { Invoices } from "./scenes/invoices";
+// import { Contacts } from "./scenes/contacts";
+// import { Bar } from "./scenes/bar";
+// import { Form } from "./scenes/form";
+// import { Line } from "./scenes/line";
+// import { Pie } from "./scenes/pie";
+// import { FAQ } from "./scenes/faq";
+// import { Geography } from "./scenes/geography";
+// import { Calendar } from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -24,6 +24,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
